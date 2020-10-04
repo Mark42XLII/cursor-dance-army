3a31a4bb4acb77b3be2a5ba6c3e1a16f663406c1
Mark42XLII <mark42xlii@gmail.com>
2020-10-04 04:46:37 -0300

    Added Tampermonkey scipt

---
+++ cursor-dance-army-tampermonkey.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         cursor-dance-army
+// @namespace    https://github.com/Mark42XLII/cursor-dance-army
+// @version      1.0
+// @description  Robot takeover for www.cursordanceparty.com, one cursor at a time!
+// @author       Mark42XLII
+// @match        http://www.cursordanceparty.com/
+// @grant        none
+// @run-at      document-start
+// ==/UserScript==
+
+window.addEventListener('beforescriptexecute',
+  function(event)
+  {
+    var originalScript = event.target;
+    console.log('script detected:', originalScript.src);
+    if('http://www.cursordanceparty.com/js/party-0.2.0.js' === originalScript.src)
+    {
+      var replacementScript = document.createElement('script');
+      replacementScript.src = 'https://raw.githubusercontent.com/Mark42XLII/cursor-dance-army/master/party.js';
+      originalScript.parentNode.replaceChild(replacementScript, originalScript);
+      event.preventDefault();
+    }
+  }
+);
